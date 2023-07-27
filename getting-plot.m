571f6b9e7ac285c79b208a5935657b38449f6068
Sona Javadi <122633840+SonaJavadi@users.noreply.github.com>
2023-07-27 16:43:03 -0600

    Add files via upload

---
+++ getting-plot.m
@@ -0,0 +1,13 @@
+clear all
+clc
+
+Data1 = load('data-2.txt');
+X = (Data1(:,1))
+Y = (Data1(:,2))
+figure(1)
+plot(X,Y,'b.')
+title('Single vs Double Dosage')
+xlabel('Half-Life')
+ylabel('Concentration')
+
+
